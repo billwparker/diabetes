8ede4ca657b329b9def89b48d1433e7cbca3be00
Bill Parker <billwparker@hotmail.com>
2025-01-01 07:26:27 -0700

    update

--- model_tensorflow.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# Define the TensorFlow neural network model
-DiabetesTFModel = tf.keras.Sequential([
-    tf.keras.layers.Dense(16, activation='relu', input_shape=(7,)),
-    tf.keras.layers.Dense(8, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
